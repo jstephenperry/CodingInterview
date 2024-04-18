3b9c2994eda6319ed0fa525b229a8920985704d7
Stephen Perry <1128766+jstephenperry@users.noreply.github.com>
2024-04-18 10:58:05 -0500

    - Added BinaryTree implementation - Added moving average calculation solutions

---
+++ CodingInterviewImplementations/Mathematical/MovingAverage.cs
@@ -0,0 +1,77 @@
+﻿namespace CodingInterviewImplementations.Mathematical
+{
+    public class MovingAverage
+    {
+        /// <summary>
+        /// Calculate the moving average of three elements at a time in an array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static double[] CalculateMovingAverage(int[] arr)
+        {
+            double[] result = new double[arr.Length - 2];
+            for (int i = 0; i < arr.Length - 2; i++)
+            {
+                result[i] = (arr[i] + arr[i + 1] + arr[i + 2]) / 3.0;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculate the moving average of three elements at a time in a list
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        public static List<double> CalculateMovingAverage(List<int> arr)
+        {
+            List<double> result = new();
+            for (int i = 0; i < arr.Count - 2; i++)
+            {
+                result.Add((arr[i] + arr[i + 1] + arr[i + 2]) / 3.0);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculate the moving average of n elements at a time in an array
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double[] CalculateMovingAverage(int[] arr, int n)
+        {
+            double[] result = new double[arr.Length - n + 1];
+            for (int i = 0; i < arr.Length - n + 1; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += arr[i + j];
+                }
+                result[i] = sum / n;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Calculate the moving average of n elements at a time in a list
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static List<double> CalculateMovingAverage(List<int> arr, int n)
+        {
+            List<double> result = new();
+            for (int i = 0; i < arr.Count - n + 1; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += arr[i + j];
+                }
+                result.Add(sum / n);
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingInterviewTests/Mathematical/MovingAverageTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CodingInterviewImplementations.Mathematical.Tests
+{
+    [TestFixture()]
+    public class MovingAverageTests
+    {
+        [Test]
+        public void CalculateMovingAverageTestWithArray()
+        {
+            Assert.That(MovingAverage.CalculateMovingAverage(new int[] { 1, 2, 3, 4, 5, 6 }), Is.EqualTo(new double[] { 2, 3, 4, 5 }));
+            Assert.That(MovingAverage.CalculateMovingAverage(new int[] { 1, 2, 3, 4, 5, 6 }, 2), Is.EqualTo(new double[] { 1.5, 2.5, 3.5, 4.5, 5.5 }));
+        }
+
+        [Test]
+        public void CalculateMovingAverageTestWithList()
+        {
+            Assert.That(MovingAverage.CalculateMovingAverage(new List<int> { 1, 2, 3, 4, 5, 6 }), Is.EqualTo(new List<double> { 2, 3, 4, 5 }));
+            Assert.That(MovingAverage.CalculateMovingAverage(new List<int> { 1, 2, 3, 4, 5, 6 }, 2), Is.EqualTo(new List<double> { 1.5, 2.5, 3.5, 4.5, 5.5 }));
+        }
+    }
+}
